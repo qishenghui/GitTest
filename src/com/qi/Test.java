61f0c5c3cfd8ca0e080f2365dc13280e28929029
qishenghui <226899@aliyun.com>
2019-05-28 11:08:51 +0800

    增加了亚索台词~~~

--- src/com/qi/Test.java
+++ src/com/qi/Test.java
@@ -3,5 +3,6 @@ package com.qi;
 public class Test {
     public static void main(String[] args) {
         System.out.println("我于杀戮之中盛放 亦如黎明中的花朵~~~");
+        System.out.println("死亡如风 常伴吾身~~~");
     }
 }
