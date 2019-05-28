acc07c8d341c4f035ef6dd3d14b1fa4a075a4e1b
qishenghui <226899@aliyun.com>
2019-05-28 14:30:45 +0800

    增加鳄鱼台词~~~!!!

--- src/com/qi/Test.java
+++ src/com/qi/Test.java
@@ -5,6 +5,6 @@ public class Test {
         System.out.println("我于杀戮之中盛放 亦如黎明中的花朵~~~");
         System.out.println("死亡如风 常伴吾身~~~");
         System.out.println("福无双至 祸不单行~~~");
-        System.out.println("少说废话 多咬几下~~~！");
+        System.out.println("少说废话 多咬几下~~~");
     }
 }
