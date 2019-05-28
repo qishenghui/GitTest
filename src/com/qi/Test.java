0c86e60295603967e3fb791c6a5b7fac800bcd6b
qishenghui <51064327+qishenghui@users.noreply.github.com>
2019-05-28 11:42:53 +0800

    增加铁男台词~~~

--- src/com/qi/Test.java
+++ src/com/qi/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我于杀戮之中盛放 亦如黎明中的花朵~~~");
         System.out.println("死亡如风 常伴吾身~~~");
+        System.out.println("福无双至 祸不单行~~~");
     }
 }
